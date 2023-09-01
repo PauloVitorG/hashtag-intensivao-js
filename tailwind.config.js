448bc8f3c4f74442f96a04e785d70318e58746b0
PauloVitorG <102983409+PauloVitorG@users.noreply.github.com>
2023-09-01 20:04:19 -0300

    Revert "tailwind"
    This reverts commit 312c5b35397b21c3d584cfe23a73295f50b4a4a7.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-=======
   content: [],
->>>>>>> parent of f1fe2d0 (delet)
   theme: {
     extend: {},
   },
