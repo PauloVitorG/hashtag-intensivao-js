f465c08bd9db9df352ad88519d0000c73336be67
PauloVitorG <102983409+PauloVitorG@users.noreply.github.com>
2023-09-01 20:03:31 -0300

    lesgo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+=======
+  content: [],
+>>>>>>> parent of f1fe2d0 (delet)
   theme: {
     extend: {},
   },
